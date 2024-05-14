cea75cb13e0176d7312a37f9dbd13f6f80fd0404
Igor <igor.torquato.20@gmail.com>
2024-05-14 18:39:08 -0300

    add: Projeto Conversor de Moedas

---
+++ script.js
@@ -0,0 +1,70 @@
+//Pegando botão:
+const button = document.getElementById("button")
+//Pegando select:
+const select = document.getElementById("currency-select")
+
+//Definindo valores fixos:
+const dolar = 5.14
+const euro = 5.56
+const bitcoin = 318435.35
+
+const convertValues = () =>{
+    //Pegando valor do input
+    const input = document.getElementById("input-value").value
+    //Pegando valor em real mostrado abaixo da img
+    const realValue = document.getElementById("real-value")
+    //Pegando valor da moeda após conversão
+    const convertedValue = document.getElementById("convert-value")
+
+    //Alterando o número mostrado de valor real com base no input
+
+    realValue.innerHTML = new Intl.NumberFormat('pt-BR',{
+        style: 'currency',
+        currency: 'BRL'
+    }).format(input)
+
+    //Fazendo cálculo
+    if(select.value === "U$ Dolár americano"){
+        convertedValue.innerHTML = new Intl.NumberFormat('en-US',{
+            style: 'currency',
+            currency: 'USD'
+        }).format(input/dolar)
+    }
+
+    if(select.value === "€ Euro"){
+        convertedValue.innerHTML = new Intl.NumberFormat('de-DE',{
+            style: 'currency',
+            currency: 'EUR'
+        }).format(input/euro)
+    }
+
+    if (select.value ==="Bitcoin"){
+        convertedValue.innerHTML = (input/bitcoin)
+    }
+}
+
+const changeCurrency = () => {
+    const currentCurrency = document.getElementById("current-currency")
+    const currentImg = document.getElementById("current-img")
+
+    if(select.value === "€ Euro"){
+        currentCurrency.innerHTML = 'Euro'
+        currentImg.src = "./assets/euro.png"
+    }
+
+    else if(select.value ==="Bitcoin"){
+        currentCurrency.innerHTML = 'Bitcoin'
+        currentImg.src = "./assets/bitcoin.png"
+    }
+
+    else if(select.value === "U$ Dolár americano"){
+        currentCurrency.innerHTML = 'Dolár'
+        currentImg.src = "./assets/estados-unidos (1) 1.png"
+    }
+
+    convertValues()
+}
+
+//Pegando o evento de click do botão e chamando uma função quando isso ocorrer
+button.addEventListener('click',convertValues)
+select.addEventListener('change', changeCurrency)
